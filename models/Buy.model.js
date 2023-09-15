f898a5b3f0b72f5cfab2a8b59ddb268a723cc81d
iuryflores <iury.flores@gmail.com>
2023-09-15 18:34:56 -0300

    add imei_id to buyModel

--- models/Buy.model.js
+++ models/Buy.model.js
@@ -2,7 +2,7 @@ import { Schema, model } from "mongoose";
 
 const buys = new Schema(
   {
-    imei_id: { type: Schema.Types.ObjectId, ref: "Imeis" },
+    imei_id: [{ type: Schema.Types.ObjectId, ref: "Imeis" }],
     fornecedor_id: { type: Schema.Types.ObjectId, ref: "Fornecedores" },
     status: { type: Boolean, default: true },
     dateBuy: { type: Date, default: Date.now },
