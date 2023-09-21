a485d54f1669ee404829dd102bc70ab05b73b63a
iuryflores <iury.flores@gmail.com>
2023-09-21 14:38:07 -0300

    swap atributes buy and imei models

--- models/Buy.model.js
+++ models/Buy.model.js
@@ -3,7 +3,10 @@ import { Schema, model } from "mongoose";
 const buys = new Schema(
   {
     imei_id: [{ type: Schema.Types.ObjectId, ref: "Imeis" }],
-    fornecedor_id: { type: Schema.Types.ObjectId, ref: "Fornecedores" },
+    description: { type: String, required: true },
+    brand: { type: String, required: true },
+    price: { type: Number },
+    fornecedor_id: { type: Schema.Types.ObjectId, ref: "Fornecedor" },
     status: { type: Boolean, default: true },
     dateBuy: { type: Date, default: Date.now },
   },
--- models/Imei.model.js
+++ models/Imei.model.js
@@ -3,9 +3,6 @@ import { Schema, model } from "mongoose";
 const imeis = new Schema(
   {
     number: { type: Number, required: true },
-    description: { type: String, required: true },
-    price: { type: Number },
-    brand: { type: String, required: true },
     status: { type: Boolean, default: true },
     buy_id: { type: Schema.Types.ObjectId, ref: "Buys" },
   },
